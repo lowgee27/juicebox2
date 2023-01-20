f40ab0f5a0cf339f79033eb8fec34e962a00249c
loganfranklin <loganfranklin@hotmail.com>
2023-01-19 20:23:20 -0600

    Part2

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const postsRouter = express.Router();
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
+});
 
 
 const { getAllPosts } = require('../db');
